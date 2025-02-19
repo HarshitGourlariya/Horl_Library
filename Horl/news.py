e073b7022b79d05bca9bff638caf97e060e3a924
HarshitGourlariya <hgourlariya@gmail.com>
2025-02-19 13:34:12 +0530

    Updated news.py

--- Horl/news.py
+++ Horl/news.py
@@ -9,3 +9,23 @@ def news(Content,Api_key):
     except:
         result = "Error"
     return result
+
+
+import requests
+
+def Country(Country,Api_key):
+    try:
+        url = f"https://api.worldnewsapi.com/top-news?source-country={Country}&language=en&date=2024-05-29"
+        api_key = Api_key
+
+        headers = {
+            'x-api-key': api_key
+        }
+
+        response = requests.get(url, headers=headers)
+        result = response.json()
+    except:
+        result = "Error"
+    return result
+
+
